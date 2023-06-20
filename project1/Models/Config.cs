91a4acd7181d7fa2c9240642c64fc205697b7cc5
MICHAEL DAVID MORALES CUBILLO <mmorales16@gmail.com>
2023-06-19 20:53:15 -0600

    Create and Read

---
+++ project1/Models/Config.cs
@@ -0,0 +1,22 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace project1.Models
+{
+    public static class Config
+    {
+        private static string connectionString = "server=saacapps.com;UserID=saacapps_ucatolica;Database=saacapps_training;Password=Ucat0lica";
+
+        /// <summary>
+        /// Manage the Db connection
+        /// </summary>
+        /// <returns> A DB connection objet like MySqlConnection</returns>
+        public static MySqlConnection GetConnection()
+        {
+            return new MySqlConnection(connectionString);
+        }
+    }
+}
---
+++ project1/Models/DAO/UserDAO.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MySql.Data.MySqlClient;
+using project1.Models.DTO;
+
+namespace project1.Models.DAO
+{
+    public class UserDAO
+    {
+        
+        public string InsertUser(UserDTO user)
+        {
+            string response = "Failed";
+
+            try
+            {
+                using (MySqlConnection connection = Config.GetConnection())
+                {
+                    connection.Open();
+
+                    string insertQuery = "INSERT INTO Users (name, email) VALUES (@pName, @pEmail)";
+
+                    using (MySqlCommand command = new MySqlCommand(insertQuery, connection))
+                    {
+                        command.Parameters.AddWithValue("@pName", user.Name);
+                        command.Parameters.AddWithValue("@pEmail", user.Email);
+
+                        int rowsAffected = command.ExecuteNonQuery();
+
+                        if (rowsAffected > 0)
+                        {
+                            response = "Success";
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error in project1.Models.DAO.UserDAO.InserUser:" + ex.Message);
+            }
+            return response;
+        }    
+        public List<UserDTO> ReadUsers()
+        {
+            List<UserDTO> users = new List<UserDTO>();
+            try
+            {
+                using (MySqlConnection connection = Config.GetConnection())
+                {
+                    connection.Open();
+
+                    string selectQuery = "SELECT * FROM Users";
+                    using (MySqlCommand command = new MySqlCommand(selectQuery, connection))
+                    {
+                       using (MySqlDataReader reader =command.ExecuteReader())
+                            while (reader.Read())
+                            {
+                                UserDTO user = new UserDTO();
+                                user.Id = reader.GetInt32("Id");
+                                user.Name = reader.GetString("name");
+                                user.Email = reader.GetString("email");
+                                users.Add(user);
+                            }
+                    }
+                }
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error in project1.Models.DAO.UserDAO.InserUser:" + ex.Message);
+            }
+            return users;
+        }
+
+        public string UpdateUser(UserDTO user, int id)
+        {
+            return null;
+        }
+
+
+        public string DeleteUser(int id)
+        {
+            return null;
+        }
+    }
+}
